4181d98b21d1f86d9b8a28801efe411c94b8d949
michota <109215822+Michota@users.noreply.github.com>
2026-03-15 14:32:25 +0100

    feat: add database configuration and environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,17 @@
-import { Module } from '@nestjs/common';
+import { MikroOrmModule } from "@mikro-orm/nestjs";
+import { Module } from "@nestjs/common";
+import { ConfigModule, ConfigType } from "@nestjs/config";
+import databaseConfig from "./config/database.config";
+import { generateMikroOrmOptions } from "./config/mikro-orm.config";
 
 @Module({
-    imports: [],
+    imports: [
+        ConfigModule.forRoot({ isGlobal: true, load: [databaseConfig] }),
+        MikroOrmModule.forRootAsync({
+            useFactory: (config: ConfigType<typeof databaseConfig>) => generateMikroOrmOptions(config),
+            inject: [databaseConfig.KEY],
+        }),
+    ],
     controllers: [],
     providers: [],
 })
