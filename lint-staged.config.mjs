315e72e60ca2cd493008fda64b96058af54b184d
michota <109215822+Michota@users.noreply.github.com>
2026-03-16 13:14:42 +0100

    feat: add pre-push hook for type-check and simplify lint-staged config

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,9 +1,5 @@
 export default {
-    '*.ts': [
-        'pnpm run lint',
-        'pnpm run format',
-        () => 'pnpm run type-check',
-    ],
-    '*.js': ['pnpm run lint', 'pnpm run format'],
-    '*.json': ['pnpm run format'],
+    "*.ts": ["pnpm run lint", "pnpm run format"],
+    "*.js": ["pnpm run lint", "pnpm run format"],
+    "*.json": ["pnpm run format"],
 };
