04756f6801730e8f2839bfcd87d314b42032e8d2
michota <109215822+Michota@users.noreply.github.com>
2026-03-17 18:50:08 +0100

    feat: add GlobalPipes to correctly transform DTOs

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import "tsconfig-paths/register"; // <-- Must be first import to work with tsconfig paths
 import { NestFactory } from "@nestjs/core";
+import { ValidationPipe } from "@nestjs/common";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
     await app.listen(process.env.PORT ?? 3000);
 }
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
