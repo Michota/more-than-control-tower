7931ecad065f0bb27215c0d21b01d10db4cc9f1e
michota <109215822+Michota@users.noreply.github.com>
2026-03-17 18:51:56 +0100

    feat: add basic Swagger configuration

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,17 @@ import "tsconfig-paths/register"; // <-- Must be first import to work with tscon
 import { NestFactory } from "@nestjs/core";
 import { ValidationPipe } from "@nestjs/common";
 import { AppModule } from "./app.module";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
     app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
+
+    const config = new DocumentBuilder().setVersion("1.0").build();
+    const documentFactory = () => SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup("api", app, documentFactory);
+
     await app.listen(process.env.PORT ?? 3000);
 }
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
