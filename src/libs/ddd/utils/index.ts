cd323b91bcda4ee8e248717a7897b8250a9bf375
michota <109215822+Michota@users.noreply.github.com>
2026-03-19 19:10:45 +0100

    feat: PropertiesOf & ValueOf

--- src/libs/ddd/utils/index.ts
+++ src/libs/ddd/utils/index.ts
@@ -1,1 +1,3 @@
 export * from "./convert-properties-to-object";
+export * from "./properties-of";
+export * from "./value-of";
---
+++ src/libs/ddd/utils/value-of.ts
@@ -0,0 +1,7 @@
+import { ValueObject } from "../value-objects";
+
+/**
+ * Extracts the properties of a Value Object. If the Value Object is a Domain Primitive, it extracts the value instead.
+ */
+
+export type ValueOf<T extends ValueObject<any>> = T extends ValueObject<infer V> ? V : never;
