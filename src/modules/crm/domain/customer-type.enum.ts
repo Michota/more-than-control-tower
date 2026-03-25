c436057ed84384c7289403a6830881c36d7aa393
michota <109215822+Michota@users.noreply.github.com>
2026-03-25 19:05:59 +0100

    feat(crm): add CustomerType enum and extend aggregate with type-specific fields
    Adds B2C/B2B/B2A customer types with Zod refinements enforcing
    required fields per type. CustomerType is immutable after creation.
    Adds update() method and note field.

---
+++ src/modules/crm/domain/customer-type.enum.ts
@@ -0,0 +1,5 @@
+export enum CustomerType {
+    B2C = "b2c",
+    B2B = "b2b",
+    B2A = "b2a",
+}
--- src/modules/crm/domain/customer.aggregate.ts
+++ src/modules/crm/domain/customer.aggregate.ts
@@ -3,14 +3,40 @@ import { EntityProps } from "../../../libs/ddd/entities/entity.abstract.js";
 import z from "zod";
 import { CustomerAddress } from "./customer-address.value-object.js";
 import { CustomerContact } from "./customer-contact.value-object.js";
+import { CustomerType } from "./customer-type.enum.js";
 import { CustomerCreatedDomainEvent } from "./events/customer-created.domain-event.js";
 
-const customerSchema = z.object({
-    name: z.string().min(1),
-    description: z.string().optional(),
-    addresses: z.array(z.instanceof(CustomerAddress)),
-    contacts: z.array(z.instanceof(CustomerContact)).min(1, "Customer must have at least one contact"),
-});
+const customerSchema = z
+    .object({
+        name: z.string().min(1),
+        customerType: z.enum(CustomerType),
+        description: z.string().optional(),
+        note: z.string().optional(),
+        firstName: z.string().optional(),
+        lastName: z.string().optional(),
+        companyName: z.string().optional(),
+        nip: z.string().optional(),
+        addresses: z.array(z.instanceof(CustomerAddress)),
+        contacts: z.array(z.instanceof(CustomerContact)).min(1, "Customer must have at least one contact"),
+    })
+    .refine(
+        (data) => {
+            if (data.customerType === CustomerType.B2C) {
+                return !!data.firstName && !!data.lastName;
+            }
+            return true;
+        },
+        { message: "B2C customers must have firstName and lastName" },
+    )
+    .refine(
+        (data) => {
+            if (data.customerType === CustomerType.B2B) {
+                return !!data.companyName && !!data.nip;
+            }
+            return true;
+        },
+        { message: "B2B customers must have companyName and nip" },
+    );
 
 export type CustomerProperties = z.infer<typeof customerSchema>;
 
@@ -38,12 +64,41 @@ export class CustomerAggregate extends AggregateRoot<CustomerProperties> {
         customerSchema.parse(this.properties);
     }
 
+    update(props: Partial<Omit<CustomerProperties, "customerType">>): void {
+        Object.assign(this.properties, props);
+        this.validate();
+    }
+
     get name(): string {
         return this.properties.name;
     }
 
+    get customerType(): CustomerType {
+        return this.properties.customerType;
+    }
+
     get description(): string | undefined {
         return this.properties.description;
+    }
+
+    get note(): string | undefined {
+        return this.properties.note;
+    }
+
+    get firstName(): string | undefined {
+        return this.properties.firstName;
+    }
+
+    get lastName(): string | undefined {
+        return this.properties.lastName;
+    }
+
+    get companyName(): string | undefined {
+        return this.properties.companyName;
+    }
+
+    get nip(): string | undefined {
+        return this.properties.nip;
     }
 
     get addresses(): CustomerAddress[] {
